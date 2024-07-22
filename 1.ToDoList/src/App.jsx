0ae281f95aa870b1ad4b4b5b895dcc4b1d6e982f
keita <dmwwvn@gmail.com>
2024-07-23 00:38:23 +0900

    🎨 Exercise Solution

--- 1.ToDoList/src/App.jsx
+++ 1.ToDoList/src/App.jsx
@@ -3,15 +3,18 @@ import React from "react";
 import "./css/App.css";
 import TaskForm from "./components/TaskForm";
 import TaskColumn from "./components/TaskColumn";
+import TodoIcon from "./assets/target.png";
+import DoingIcon from "./assets/star.png";
+import DoneIcon from "./assets/check.png";
 
 const App = () => {
   return (
     <div className="app">
       <TaskForm />
       <main className="app_main">
-        <TaskColumn />
-        <TaskColumn />
-        <TaskColumn />
+        <TaskColumn title="To do" icon={TodoIcon} />
+        <TaskColumn title="Doing" icon={DoingIcon} />
+        <TaskColumn title="Done" icon={DoneIcon} />
       </main>
     </div>
   );
