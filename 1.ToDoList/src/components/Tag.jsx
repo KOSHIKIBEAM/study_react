75e331662f99a5c33db1280b308b17e67272490b
keita <dmwwvn@gmail.com>
2024-07-23 05:39:25 +0900

    Display selected tag on UI

--- 1.ToDoList/src/components/Tag.jsx
+++ 1.ToDoList/src/components/Tag.jsx
@@ -2,9 +2,21 @@ import React from "react";
 
 import "../css/Tag.css";
 
-const Tag = ({ selectTag, tagName }) => {
+const Tag = ({ selectTag, tagName, selected }) => {
+  const tagStyle = {
+    HTML: { backgroundColor: "#fda821" },
+    CSS: { backgroundColor: "#15d4c8" },
+    JavaScript: { backgroundColor: "#ffd12c" },
+    React: { backgroundColor: "#4cdafc" },
+    default: { backgroundColor: "#f0f0f0" },
+  };
   return (
-    <button type="button" className="tag" onClick={() => selectTag(tagName)}>
+    <button
+      type="button"
+      className="tag"
+      style={selected ? tagStyle[tagName] : tagStyle.default}
+      onClick={() => selectTag(tagName)}
+    >
       {tagName}
     </button>
   );
--- 1.ToDoList/src/components/TaskForm.jsx
+++ 1.ToDoList/src/components/TaskForm.jsx
@@ -10,6 +10,10 @@ const TaskForm = () => {
     tags: [],
   });
 
+  const checkTag = (tag) => {
+    return taskData.tags.some((item) => item === tag);
+  };
+
   const selectTag = (tag) => {
     if (taskData.tags.some((item) => item === tag)) {
       const filterTags = taskData.tags.filter((item) => item !== tag);
@@ -52,10 +56,26 @@ const TaskForm = () => {
 
         <div className="task_form_bottom_line">
           <div>
-            <Tag tagName="HTML" selectTag={selectTag} />
-            <Tag tagName="CSS" selectTag={selectTag} />
-            <Tag tagName="JavaScript" selectTag={selectTag} />
-            <Tag tagName="React" selectTag={selectTag} />
+            <Tag
+              tagName="HTML"
+              selectTag={selectTag}
+              selected={checkTag("HTML")}
+            />
+            <Tag
+              tagName="CSS"
+              selectTag={selectTag}
+              selected={checkTag("CSS")}
+            />
+            <Tag
+              tagName="JavaScript"
+              selectTag={selectTag}
+              selected={checkTag("JavaScript")}
+            />
+            <Tag
+              tagName="React"
+              selectTag={selectTag}
+              selected={checkTag("React")}
+            />
           </div>
           <div>
             <select
