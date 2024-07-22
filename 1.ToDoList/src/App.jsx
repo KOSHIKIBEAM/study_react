e298350e5e07ea3c9ec2daff18994cd5a29bd070
keita <dmwwvn@gmail.com>
2024-07-23 00:26:53 +0900

    🎨 Building Task List Component

--- 1.ToDoList/src/App.jsx
+++ 1.ToDoList/src/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 import "./css/App.css";
 import TaskForm from "./components/TaskForm";
+import TaskColumn from "./components/TaskColumn";
 
 const App = () => {
   return (
     <div className="app">
       <TaskForm />
       <main className="app_main">
-        <section className="task_column">section1</section>
-        <section className="task_column">section2</section>
-        <section className="task_column">section3</section>
+        <TaskColumn />
+        <TaskColumn />
+        <TaskColumn />
       </main>
     </div>
   );
